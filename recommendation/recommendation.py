f62712413e158558f6c1ac426636def858537130
Antoine Dufournet <antoine.dufournet@student-cs.fr>
2022-06-08 10:42:22 +0200

    Ajout de la liste des genres

---
+++ recommendation/recommendation.py
@@ -0,0 +1,35 @@
+# On va s'interesser ici au genre des films, que l'on retrouve dans genre_ids.
+# genre_ids: Array(3)
+# 0: 14
+# 1: 12
+# 2: 28
+
+{
+    "genres": [
+        {"id": 28, "name": "Action"},
+        {"id": 12, "name": "Adventure"},
+        {"id": 16, "name": "Animation"},
+        {"id": 35, "name": "Comedy"},
+        {"id": 80, "name": "Crime"},
+        {"id": 99, "name": "Documentary"},
+        {"id": 18, "name": "Drama"},
+        {"id": 10751, "name": "Family"},
+        {"id": 14, "name": "Fantasy"},
+        {"id": 36, "name": "History"},
+        {"id": 27, "name": "Horror"},
+        {"id": 10402, "name": "Music"},
+        {"id": 9648, "name": "Mystery"},
+        {"id": 10749, "name": "Romance"},
+        {"id": 878, "name": "Science Fiction"},
+        {"id": 10770, "name": "TV Movie"},
+        {"id": 53, "name": "Thriller"},
+        {"id": 10752, "name": "War"},
+        {"id": 37, "name": "Western"},
+    ]
+}
+
+
+# GET/movie/{movie_id}/similar
+# Get a list of similar movies. This is not the same as the "Recommendation" system you see on the website.
+# These items are assembled by looking at keywords and genres.
+# https://api.themoviedb.org/3/movie/{movie_id}/similar?api_key=<<api_key>>&page=1
