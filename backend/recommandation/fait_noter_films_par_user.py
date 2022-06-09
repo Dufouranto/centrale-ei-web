de53616d94c4aa83c52caea77d68d7bacf971ffa
Antoine Dufournet <antoine.dufournet@student-cs.fr>
2022-06-09 16:22:13 +0200

    fin de l'algorithme de recommandation

--- backend/recommandation/fait_noter_films_par_user.py
+++ backend/recommandation/fait_noter_films_par_user.py
@@ -4,6 +4,9 @@ from random import randint
 import requests as req
 
 dbname = get_database()
+
+dbname.likes.drop()
+
 collection_users = dbname["users"]
 cursor_users = collection_users.find()
 users = pd.DataFrame(list(cursor_users))
@@ -15,10 +18,10 @@ movies = pd.DataFrame(list(cursor_movies))
 list_movies_id = movies["_id"]
 nb_movies = len(list_movies_id)
 
-for userId in users["_id"][:100]:
-    for i in range(6):
+for userId in users["_id"]:
+    for i in range(10):
         p = randint(0, nb_movies - 1)
         note = randint(0, 5)
         movieId = list_movies_id[p]
-        url = "http://localhost:3000/" + str(userId) + str(movieId)
+        url = "http://localhost:3000/users/" + str(userId) + "/" + str(movieId)
         req.post(url, None, {"mark": note})
--- backend/recommandation/recommandation.py
+++ backend/recommandation/recommandation.py
@@ -1,3 +1,163 @@
+from pymongo_test import get_database
+from random import choice
+from sklearn import model_selection
+from sklearn.metrics.pairwise import pairwise_distances
+import numpy as np
+import pandas as pd
+
+
+def get_indexes_max_value(l):
+    return [i for i, x in enumerate(l) if x == max(l)], max(l)
+
+
+def negation_list(list1, list2):
+    l = []
+    for i in list1:
+        if np.all(list2 != i):
+            l.append(i)
+    return l
+
+
+def recommandation_similar():
+    # Get the database using the method we defined in pymongo_test_insert file
+    dbname = get_database()
+
+    L = []
+    for i in ["likes", "movies", "users"]:
+        # Connexion to the collection movies
+        collection_i = dbname[i]
+
+        cursor_i = collection_i.find()
+
+        # Expand the cursor and construct the DataFrame
+        L.append(pd.DataFrame(list(cursor_i)))
+
+    likes, movies, users = L
+
+    nb_users = users.email.unique().shape[0]
+    nb_movies = movies.title.unique().shape[0]
+
+    print("Number of users : ", nb_users)
+    print("Number of movies : ", nb_movies)
+    print()
+
+    train_likes, test_likes = model_selection.train_test_split(likes, test_size=0.25)
+
+    print(train_likes.head())
+
+    movie_matrix = train_likes.pivot_table(
+        index="userId", columns="movieId", values="mark"
+    )
+    print("movie_matrix")
+    print()
+    print(movie_matrix)
+
+    # test_matrix = test_likes.pivot_table(
+    #     index="userId", columns="movieId", values="mark"
+    # )
+    # print()
+    # print("test_likes")
+    # print()
+    # print(test_likes)
+
+    # Constucrion de la matrice contenant que les notes des différents films.
+    train_likes_matrix = movie_matrix.to_numpy()
+    for i in range(len(train_likes_matrix)):
+        for j in range(len(train_likes_matrix[0])):
+            if np.isnan(train_likes_matrix[i][j]):
+                train_likes_matrix[i][j] = 0
+
+    print("train_likes_matrix")
+    print(train_likes_matrix)
+
+    # Construction de la matrice de test contenant que les notes des différents films.
+    # test_likes_matrix = test_matrix.to_numpy()
+    # for i in range(len(test_likes_matrix)):
+    #     for j in range(len(test_likes_matrix[0])):
+    #         if np.isnan(test_likes_matrix[i][j]):
+    #             test_likes_matrix[i][j] = 0
+
+    # print("test_likes_matrix")
+    # print(test_likes_matrix)
+
+    user_similarity = pairwise_distances(train_likes_matrix, metric="cosine")
+
+    print("user_similarity")
+    print(user_similarity)
+
+    dbname.recommandations.drop()
+    collection_recommandations = dbname["recommandations"]
+    collection_likes = dbname["likes"]
+
+    for i in range(len(user_similarity)):
+        user_index, score = get_indexes_max_value(user_similarity[i])
+        user_index, score = choice(user_index), score - 0.01
+        idSimilarUser = movie_matrix.index[user_index]
+        idUser = movie_matrix.index[i]
+
+        movies_recommanded = collection_likes.find(
+            {"userId": idSimilarUser}, {"movieId": 1, "_id": 0}
+        )
+        movies_already_seen = collection_likes.find(
+            {"userId": idUser}, {"movieId": 1, "_id": 0}
+        )
+        list1 = (pd.DataFrame(list(movies_recommanded)).to_numpy(),)
+        list2 = (pd.DataFrame(list(movies_already_seen)).to_numpy(),)
+
+        list_id_movies_reco = negation_list(list1, list2)
+
+        if list_id_movies_reco:
+            for objId in list_id_movies_reco[0]:
+                collection_recommandations.insert_one(
+                    {"userId": idUser, "movieId": objId[0], "score": score}
+                )
+
+
+def recommandation_simple(user_id):
+    # Get all the ids of the films the user noted
+
+    dbname = get_database()
+
+    collection_likes = dbname["likes"]
+
+    movies_noted = collection_likes.find({"userId": user_id})
+
+    print(movies_noted)
+    # Find the genre_ids of these films
+
+    # Sort the genres according to there occurences
+
+    # Return a list of film ids for each genre
+    return None
+
+
+recommandation_similar()
+# recommandation_simple(15845)
+
+# GET/movie/{movie_id}/similar
+# Get a list of similar movies. This is not the same as the "Recommendation" system you see on the website.
+# These items are assembled by looking at keywords and genres.
+# https://api.themoviedb.org/3/movie/{movie_id}/similar?api_key=<<api_key>>&page=1
+
+# Likes Table
+likes_cols = ["userId", "movieId", "mark"]
+
+# Movies Table
+movies_cols = [
+    "title",
+    "desc",
+    "url",
+    "popularity",
+    "genre_ids",
+    "release_date",
+    "poster_path",
+]
+# Recommandation Table
+recommandation_cols = ["userId", "movieId", "score"]
+
+# Users Table
+users_cols = ["email", "firstName", "lastName"]
+
 # On va s'interesser ici au genre des films, que l'on retrouve dans genre_ids.
 # genre_ids: Array(3)
 
@@ -26,104 +186,3 @@
     ]
 }
 """
-
-
-from pymongo_test import get_database
-from sklearn import model_selection
-from sklearn.metrics.pairwise import pairwise_distances
-import numpy as np
-import pandas as pd
-
-# Likes Table
-likes_cols = ["userId", "movieId", "mark"]
-
-# Movies Table
-movies_cols = [
-    "title",
-    "desc",
-    "url",
-    "popularity",
-    "genre_ids",
-    "release_date",
-    "poster_path",
-]
-# Recommandation Table
-recommandation_cols = ["userId", "movieId", "score"]
-
-# Users Table
-users_cols = ["email", "firstName", "lastName"]
-
-
-def recommandation_similar():
-    # Get the database using the method we defined in pymongo_test_insert file
-    dbname = get_database()
-
-    L = []
-    for i in ["likes", "movies", "recommandations", "users"]:
-        # Connexion to the collection movies
-        collection_i = dbname[i]
-
-        cursor_i = collection_i.find()
-
-        # Expand the cursor and construct the DataFrame
-        L.append(pd.DataFrame(list(cursor_i)))
-
-    likes, movies, recommandations, users = L
-
-    nb_users = users.email.unique().shape[0]
-    nb_items = movies.title.unique().shape[0]
-
-    print("Number of users : ", nb_users)
-    print("Number of movies : ", nb_items)
-
-    train_likes, test_likes = model_selection.train_test_split(likes, test_size=0.25)
-
-    movie_matrix = train_likes.pivot_table(
-        index="userId", columns="movieId", values="mark"
-    )
-    print("movie_matrix")
-    movie_matrix.head()
-
-    # Constucrion de la matrice contenant que les notes des différents films.
-    train_likes_matrix = np.zeros((nb_users, nb_items))
-    for line in train_likes.itertuples():
-        train_likes_matrix[line[1] - 1, line[2] - 1] = line[3]
-    print("train_likes_matrix")
-    print(train_likes_matrix)
-
-    # Construction de la matrice de test contenant que les notes des différents films.
-    test_likes_matrix = np.zeros((nb_users, nb_items))
-    for line in test_likes.itertuples():
-        test_likes_matrix[line[1] - 1, line[2] - 1] = line[3]
-
-    user_similarity = pairwise_distances(train_likes_matrix, metric="cosine")
-
-    print("user_similarity")
-    print(user_similarity)
-
-
-def recommandation_simple(user_id):
-    # Get all the ids of the films the user noted
-
-    dbname = get_database()
-
-    collection_likes = dbname["likes"]
-
-    movies_noted = collection_likes.find({"userId": user_id})
-
-    print(movies_noted)
-    # Find the genre_ids of these films
-
-    # Sort the genres according to there occurences
-
-    # Return a list of film ids for each genre
-    return None
-
-
-recommandation_similar()
-recommandation_simple(15845)
-
-# GET/movie/{movie_id}/similar
-# Get a list of similar movies. This is not the same as the "Recommendation" system you see on the website.
-# These items are assembled by looking at keywords and genres.
-# https://api.themoviedb.org/3/movie/{movie_id}/similar?api_key=<<api_key>>&page=1
