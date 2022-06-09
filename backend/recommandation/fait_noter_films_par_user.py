de6c84f4220641502fe0a642d0136e7e2c0fd0fe
Antoine Dufournet <antoine.dufournet@student-cs.fr>
2022-06-09 10:47:00 +0200

    ajout des films notés

--- backend/recommandation/fait_noter_films_par_user.py
+++ backend/recommandation/fait_noter_films_par_user.py
@@ -1,10 +1,24 @@
 from pymongo_test import get_database
 import pandas as pd
+from random import randint
+import requests as req
 
 dbname = get_database()
 collection_users = dbname["users"]
 cursor_users = collection_users.find()
 users = pd.DataFrame(list(cursor_users))
-print(users)
-for i in range(100):
-    a = 1
+
+collection_movies = dbname["movies"]
+cursor_movies = collection_movies.find()
+movies = pd.DataFrame(list(cursor_movies))
+
+list_movies_id = movies["_id"]
+nb_movies = len(list_movies_id)
+
+for userId in users["_id"][:100]:
+    for i in range(6):
+        p = randint(0, nb_movies - 1)
+        note = randint(0, 5)
+        movieId = list_movies_id[p]
+        url = "http://localhost:3000/" + str(userId) + str(movieId)
+        req.post(url, None, {"mark": note})
